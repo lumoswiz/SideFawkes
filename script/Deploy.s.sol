a628b222212ed7026a243c0eed8805e1711f7f8c
lumoswiz <lumoswiz@protonmail.com>
2024-10-04 17:45:36 +0200

    added: deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,7 +3,16 @@ pragma solidity >=0.8.25 <0.9.0;
 
 import { BaseScript } from "./Base.s.sol";
 
-/// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
+import { VickreyAuction } from "src/VickreyAuction.sol";
+import { Asset } from "test/mocks/Asset.sol";
+import { Payment } from "test/mocks/Payment.sol";
+
+// forge script script/Deploy.s.sol:Deploy --rpc-url fhenix --broadcast --verify --verifier blockscout --verifier-url
+// $VERIFIER_URL --skip-simulation -vvvv
 contract Deploy is BaseScript {
-    function run() public { }
+    function run() public broadcast {
+        Asset asset = new Asset();
+        Payment payment = new Payment();
+        VickreyAuction auction = new VickreyAuction(address(payment), 10_800);
+    }
 }
